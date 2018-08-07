555ebff2d1a6c8e7013623e9639f3a3f1cae969b
João Ribeiro <joaosoft@gmail.com>
2018-08-07 23:25:17 +0100

    fix

--- errors.go
+++ errors.go
@@ -8,68 +8,60 @@ import (
 type ListErr []*Err
 
 type Err struct {
-	previous *Err
-	error    error
-	code     string
+	Previous *Err
+	Err      error
+	Code     string
 }
 
 func New(code string, err interface{}) *Err {
 
 	switch v := err.(type) {
 	case error:
-		return &Err{code: code, error: v}
+		return &Err{Code: code, Err: v}
 
 	case string:
-		return &Err{code: code, error: errors.New(v)}
+		return &Err{Code: code, Err: errors.New(v)}
 
 	default:
-		return &Err{code: code, error: errors.New(fmt.Sprint(v))}
+		return &Err{Code: code, Err: errors.New(fmt.Sprint(v))}
 
 	}
 }
 
 func (e *Err) Add(newErr *Err) {
 	prevErr := &Err{
-		previous: e.previous,
-		error:    e.error,
+		Previous: e.Previous,
+		Err:      e.Err,
 	}
 
-	e.previous = prevErr
-	e.error = newErr
-}
-
-func (e *Err) Err() error {
-	return e.error
-}
-
-func (e *Err) Code() string {
-	return e.code
+	e.Previous = prevErr
+	e.Err = newErr
 }
 
 func (e *Err) Error() string {
-	return e.error.Error()
+	return e.Err.Error()
 }
 
-func (e *Err) Errors() []error {
-	errors := make([]error, 0)
-	errors = append(errors, e.error)
+func (e *Err) Errors() []*Err {
+	errors := make([]*Err, 0)
+	errors = append(errors, e)
 
-	nextErr := e.previous
+	nextErr := e.Previous
 	for nextErr != nil {
-		errors = append(errors, e.previous.error)
-		nextErr = nextErr.previous
+		errors = append(errors, e.Previous)
+		nextErr = nextErr.Previous
 	}
 
 	return errors
 }
 
 func (e *Err) Cause() string {
-	str := fmt.Sprintf("'%s'", e.error.Error())
+	str := fmt.Sprintf("'%s'", e.Err.Error())
 
-	nextErr := e.previous
+	nextErr := e.Previous
 	for nextErr != nil {
-		str += fmt.Sprintf(", caused by '%s'", e.previous.error.Error())
-		nextErr = nextErr.previous
+		str += fmt.Sprintf(", caused by '%s'", e.Previous.Err.Error())
+		nextErr = nextErr.Previous
 	}
 	return str
 }
