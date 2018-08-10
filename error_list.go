5683e6a6a989374c8fcb30293fa3fc8974e31c2c
João Ribeiro <joaosoft@gmail.com>
2018-08-10 20:17:09 +0100

    fix

--- error_list.go
+++ error_list.go
@@ -8,7 +8,7 @@ func (e *ListErr) IsEmpty() bool {
 	return len(*e) == 0
 }
 
-func (e *ListErr) Add(err *Err) *ListErr {
+func (e *ListErr) Add(err IErr) *ListErr {
 	*e = append(*e, err)
 	return e
 }
