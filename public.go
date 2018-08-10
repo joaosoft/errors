b37f2a34d8fbfb7abe66327ea67eaaf3ca20eecf
João Ribeiro <joaosoft@gmail.com>
2018-08-10 18:16:29 +0100

    ficx

--- public.go
+++ public.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 )
 
-func New(code string, err interface{}) *Err {
+func New(code string, err interface{}, params ...interface{}) *Err {
 
 	switch v := err.(type) {
 	case error:
 		return &Err{code: code, error: v}
 
 	case string:
-		return &Err{code: code, error: errors.New(v)}
+		return &Err{code: code, error: errors.New(fmt.Sprintf(v, params))}
 
 	default:
 		return &Err{code: code, error: errors.New(fmt.Sprint(v))}
