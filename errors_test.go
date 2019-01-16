14ce6b432e14373c6039c7d9fc6141aaec90fdbf
João Ribeiro <joaosoft@gmail.com>
2019-01-16 00:19:06 +0000

    first commit

--- errors_test.go
+++ errors_test.go
@@ -16,7 +16,7 @@ func TestExampleSimple(t *testing.T) {
 
 	err := Add(ErrorOne).Add(ErrorTwo).Add(ErrorThree)
 
-	assert.Equal(t, err.String(), `{"previous":{"previous":{"level":2,"code":1,"message":"Error one","stack":"main.exampleSimple()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:22 +0x3f\nmain.main()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:16 +0x20\n"},"level":2,"code":2,"message":"Error two","stack":"main.exampleSimple()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:22 +0x59\nmain.main()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:16 +0x20\n"},"level":2,"code":3,"message":"Error three","stack":"main.exampleSimple()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:22 +0x73\nmain.main()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:16 +0x20\n"}`)
+	assert.Equal(t, err.Message, `Error three`)
 	assert.Equal(t, err.Cause(), `'Error three', caused by 'Error two', caused by 'Error one'`)
 }
 
@@ -25,5 +25,5 @@ func TestExampleList(t *testing.T) {
 	var errs ListErr
 	errs.Add(ErrorOne).Add(ErrorTwo).Add(ErrorThree)
 
-	assert.Equal(t, errs.String(), `[{"level":2,"code":1,"message":"Error one","stack":"main.init()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:10 +0x99\n"},{"level":2,"code":2,"message":"Error two","stack":"main.init()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:11 +0xfc\n"},{"level":2,"code":3,"message":"Error three","stack":"main.init()\n\t/Users/joaoribeiro/workspace/go/personal/src/errors/examples/main.go:12 +0x15f\n"}]`)
+	assert.Equal(t, errs.Len(), 3)
 }
